0c384170fd310e9f1e7acb2713fb54228fb54744
Brandon GS <cosmoscrew1@gmail.com>
2021-03-04 19:45:34 -0600

    allow deploy

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const dev = process.env.NODE_ENV !== "production";
 const prefix = "";
 
 module.exports = {
-  i18n: {
-    locales: ["en", "es"],
-    defaultLocale: "en",
-  },
   assetPrefix: !dev ? prefix : "",
   basePath: !dev ? prefix : "",
   env: {
