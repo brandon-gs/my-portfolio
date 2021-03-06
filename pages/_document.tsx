3f978a9605798f2818eca2321cdfc390cbc2cc13
Brandon GS <cosmoscrew1@gmail.com>
2021-03-05 20:29:45 -0600

    Add google analitycs and meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,51 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import theme from "utils/theme";
 import { ServerStyleSheets } from "@material-ui/core";
-import React from "react";
+import React, { Fragment } from "react";
+
+interface GoogleTags {
+  __html: string;
+}
 
 class MyDocument extends Document {
+  setGoogleTags(): GoogleTags {
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-KS0M93HBNQ')
+      `,
+    };
+  }
+
   render(): JSX.Element {
-    const prefix = this.props.__NEXT_DATA__.assetPrefix || "";
+    const isProduction = process.env.NODE_ENV === "production";
     return (
       <Html>
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="author" content="BrandonGS" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link rel="stylesheet" href={`${prefix}/css/global.css`} />
-          <link rel="stylesheet" href={`${prefix}/css/nprogress.css`} />
-          <link rel="shortcut icon" href={`${prefix}/favicon.ico`} />
+          <link rel="stylesheet" href={`/css/global.css`} />
+          <link rel="stylesheet" href={`/css/nprogress.css`} />
+          <link rel="shortcut icon" href={`/favicon.ico`} />
         </Head>
         <body>
           <Main />
           <NextScript />
+          {isProduction && (
+            <Fragment>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-KS0M93HBNQ"
+              ></script>
+              <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+            </Fragment>
+          )}
         </body>
       </Html>
     );
@@ -36,6 +61,7 @@ MyDocument.getInitialProps = async (ctx) => {
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
   const initialProps = await Document.getInitialProps(ctx);
+
   return {
     ...initialProps,
     styles: [
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -16,6 +16,14 @@ export default function ProjectsPage() {
     <Fragment>
       <Head>
         <title>Brandon García | {t.projects}</title>
+        <meta
+          name="keywords"
+          content="brandon, brandon garcia, brandongs, frontend developer, backend developer, freelance, software engineering, software engineering projects, create web apps, create mobile apps, create software"
+        />
+        <meta
+          name="description"
+          content="List of awesome software engineering projects with sort algorithms, find algorithms, data structures, social networks and more"
+        />
       </Head>
       <Navbar />
       <ListProjects />
