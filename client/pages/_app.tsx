a4b684596aa85f3dc45f8f363b1b9f8421f5bdc4
BrandonGS <brandongs180@gmail.com>
2021-03-30 02:33:46 -0600

    Add emotion

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,19 +1,28 @@
+import * as React from "react";
+// Types
+import { AppProps, AppContext, AppInitialProps } from "next/app";
+// Theme
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "utils/theme";
+// Progress load page
 import NProgress from "nprogress";
 import Router from "next/router";
-import { AppProps, AppContext, AppInitialProps } from "next/app";
-import { useEffect, FC } from "react";
+// Motion
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
+export const cache = createCache({ key: "css", prepend: true });
+
+// Config nprogress
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-const MyApp: FC<AppProps> = (props: AppProps) => {
+const MyApp: React.FC<AppProps> = (props: AppProps) => {
   const { Component, pageProps } = props;
 
-  useEffect(() => {
+  React.useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement?.removeChild(jssStyles);
@@ -21,10 +30,12 @@ const MyApp: FC<AppProps> = (props: AppProps) => {
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
 
